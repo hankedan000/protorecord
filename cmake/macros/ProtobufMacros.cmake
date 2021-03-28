be6fdc500ebc23fdabc6aa1b66807dec5bad29b5
hankedan000 <dany32412@gmail.com>
2021-03-28 00:20:50 -0400

    Fixes INTERFACE_INCLUDE_DIRECTORIES issue with protobuf export target

--- cmake/macros/ProtobufMacros.cmake
+++ cmake/macros/ProtobufMacros.cmake
@@ -10,8 +10,12 @@ macro (create_protobuf_target PROTO_FILE)
 	add_library(${PROTO_TARGET_STATIC} STATIC ${PROTO_SRCS})
 	target_link_libraries(${PROTO_TARGET_SHARED} ${Protobuf_LIBRARIES})
 	target_link_libraries(${PROTO_TARGET_STATIC} ${Protobuf_LIBRARIES})
-	target_include_directories(${PROTO_TARGET_SHARED} PUBLIC ${CMAKE_CURRENT_BINARY_DIR})
-	target_include_directories(${PROTO_TARGET_STATIC} PUBLIC ${CMAKE_CURRENT_BINARY_DIR})
-	set_target_properties(${PROTO_TARGET_SHARED} PROPERTIES  PUBLIC_HEADER ${PROTO_HDRS})
-	set_target_properties(${PROTO_TARGET_STATIC} PROPERTIES  PUBLIC_HEADER ${PROTO_HDRS})
+	target_include_directories(${PROTO_TARGET_SHARED} PUBLIC
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+		$<INSTALL_INTERFACE:include/${CMAKE_PROJECT_NAME}>)
+	target_include_directories(${PROTO_TARGET_STATIC} PUBLIC
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+		$<INSTALL_INTERFACE:include/${CMAKE_PROJECT_NAME}>)
+	set_target_properties(${PROTO_TARGET_SHARED} PROPERTIES PUBLIC_HEADER ${PROTO_HDRS})
+	set_target_properties(${PROTO_TARGET_STATIC} PROPERTIES PUBLIC_HEADER ${PROTO_HDRS})
 endmacro()
